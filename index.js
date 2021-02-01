e4feeb3e19d99e23da42cd87d3d898bc15d600f4
Swati Makhija <swatimakhija@Swatis-MacBook-Air.local>
2021-02-01 10:50:56 -0800

    add other modules

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const myModule = require("./myModule")
+const myOtherModule = require("./myOtherModule")
+const modulePractice = require("./modulePractice")
+
+
 console.log("Hello World!"); 
 
 function hello(name) {
@@ -6,3 +11,11 @@ function hello(name) {
 
 hello("MOCHA")
 hello("JC")
+
+console.log(myModule.add(2, 2))
+console.log(myModule.subtract(5,4))
+
+//my other module
+myOtherModule.welcomeToNode();
+
+modulePractice.feels("Grrrreat!")
---
+++ modulePractice.js
@@ -0,0 +1,5 @@
+const feels = (emotion) => {
+    console.log(`I feel sooo ${emotion}`)
+}
+
+module.exports = {feels}
